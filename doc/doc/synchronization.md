e043dcf2e1d4b8c49391d86b95863f706b346df9
Loïc Baumann <loic.baumann@outlook.com>
2023-11-23 15:25:40 +0100

    doc wip

---
+++ doc/doc/synchronization.md
@@ -0,0 +1,15 @@
+﻿---
+uid: synchronization-overview
+---
+
+# Synchronization overview
+
+## Why?
+The OS and .NET offer many mechanisms to take care about synchronization between threads and processes.
+The traditional `lock` of .net implemented by the [monitor](https://learn.microsoft.com/en-us/dotnet/api/system.threading.monitor) class is doing a very good job but is general purpose.
+
+🍅 implements custom types with specific features to allows very low latency synchronization between threads and/or processes. 
+These types are also designed to take full advantage of multi-core CPU and very short locking span.
+
+## AccessControl
+The [AccessControl](<xref:Tomate.AccessControl>) type allow threads to share the usage of a particular resource.
--- src/Tomate/Synchronization/AccessControl.cs
+++ src/Tomate/Synchronization/AccessControl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
+using JetBrains.Annotations;
 
 namespace Tomate;
 
@@ -9,12 +10,18 @@ namespace Tomate;
 /// Costs 8 bytes of data.
 /// </summary>
 [StructLayout(LayoutKind.Sequential)]
+[PublicAPI]
 public struct AccessControl
 {
     // This type can be used on Memory Mapped File for interprocess synchronization, so the Managed Thread Id is not enough as
     //  a discriminant, we also use the Process Id to store a unique value in _lockedByThreadId
     private static readonly int ProcessId = Process.GetCurrentProcess().Id;
 
+    /// <summary>
+    /// Constructor an instance stored in the given memory segment
+    /// </summary>
+    /// <param name="segment">The memory segment where the instance will be stored.</param>
+    /// <returns>The ref to the instance</returns>
     public static ref AccessControl Construct(MemorySegment<AccessControl> segment)
     {
         ref var ac = ref segment.AsRef();
@@ -23,6 +30,9 @@ public struct AccessControl
         return ref ac;
     }
 
+    /// <summary>
+    /// Reset the usage of the instance to its default state.
+    /// </summary>
     public void Reset()
     {
         _lockedByThreadId = 0;
@@ -33,7 +43,18 @@ public struct AccessControl
     private volatile int _lockedByThreadId;
     private volatile int _sharedUsedCounter;
 
-    public int LockedByThreadId => _lockedByThreadId;
+    /// <summary>
+    /// Returns the Id of the current Process/Thread that own the lock 
+    /// </summary>
+    /// <remarks>
+    /// <c>0</c> is returned if the Access Control is not locked.
+    /// The returned value is a xor combination of the Process Id and the threadId and should be used for information purpose only.
+    /// </remarks>
+    public int LockedById => _lockedByThreadId;
+    
+    /// <summary>
+    /// Returns the count of concurrent accesses.
+    /// </summary>
     public int SharedUsedCounter => _sharedUsedCounter;
 
     public void EnterSharedAccess()
@@ -69,7 +90,7 @@ public struct AccessControl
 
     public void EnterExclusiveAccess()
     {
-        var ct = Environment.CurrentManagedThreadId ^ ProcessId;
+        var ct = CurrentProcessThreadId;
 
         // Fast path: exclusive lock works immediately
         if (Interlocked.CompareExchange(ref _lockedByThreadId, ct, 0) == 0)
@@ -114,7 +135,7 @@ public struct AccessControl
 
     public bool TryPromoteToExclusiveAccess()
     {
-        var ct = Environment.CurrentManagedThreadId ^ ProcessId;
+        var ct = CurrentProcessThreadId;
 
         // We can enter only if we are the only user (_sharedUsedCounter == 1)
         if (_sharedUsedCounter != 1)
@@ -142,4 +163,5 @@ public struct AccessControl
     public void DemoteFromExclusiveAccess() => _lockedByThreadId = 0;
 
     public void ExitExclusiveAccess() => _lockedByThreadId = 0;
+    private static int CurrentProcessThreadId => Environment.CurrentManagedThreadId ^ ProcessId;
 }
